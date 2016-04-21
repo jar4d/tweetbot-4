21a7dc7852ab66f75a50574ba1a3edb652a63561
jar4d <jared.ruddy@gmail.com>
2016-04-21 14:02:57 +0100

    updated

--- client/client.js
+++ client/client.js
@@ -1,4 +1,11 @@
-Template.grow.events({
+  //key: 'AIzaSyC3H26C2RdRAtczTYqlLariqlDbFscfkyY'
+var goStop = stop;
+
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
+
+  Template.grow.events({
   'submit .addScrape'(event) {
     // Prevent default browser form submit
     event.preventDefault();
@@ -18,4 +25,330 @@ Template.profile.helpers({
   profileDetails: function(){
     return Profile.findOne();
   } 
+});
+
+Template.grow.helpers({
+  scrapeIdQuantity: function(){
+     var scrapes = Scraped.find({scrapeId: {$exists:true}},{}).fetch();
+     return scrapes.length;
+  }
+});
+
+Template.track.helpers({
+  scrapedDetails: function(){
+     var databaseDetails = Scraped.find({scrapeId: {$exists:true}},{}).count();
+     return databaseDetails;
+  },
+    friendsDBDetails: function(){
+     var databaseDetails = Tweets.find({friends: {$exists:true}},{}).count();
+     return databaseDetails;
+  },
+    followersDBDetails: function(){
+     var databaseDetails = Tweets.find({followers: {$exists:true}},{}).count();
+     return databaseDetails;
+  },
+});
+
+
+'change .runScriptSwitch': function(event) {
+  console.log("switch");
+  var x = event.target.checked;
+  Session.set("statevalue", x);
+  console.log(Session.get("statevalue"));
+  if(x==true){
+    Meteor.call('go');
+  }else{
+    Meteor.call('stop');    
+  }
+ }
+
+
+
+});
+
+Template.navTemplate.helpers({
+  runninghighlight: function(){
+    var goStop = !goStop;
+    Session.set('goStop', goStop);
+    if(Session.get('goStop')){
+      return "no-hover pink accent-2";
+    }else{
+      return "no-hover"
+    }
+  },
+  stoppedhighlight: function(){
+    if(!Session.get('goStop')){
+      return "no-hover pink accent-2";
+    }else{
+      return "no-hover"
+    }
+  }
+
+});
+
+
+
+
+
+
+
+
+
+Template.map.helpers({
+  exampleMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(51.5074, 0.1278),
+        zoom: 2
+      };
+    }
+  }
+});
+
+
+
+
+
+Template.map.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('exampleMap', function(map) {
+    map.instance.setOptions({styles: [
+    {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.government",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.medical",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.place_of_worship",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.school",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.sports_complex",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.station",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#15bcff"
+            },
+            {
+                "visibility": "on"
+            },
+            {
+                "weight": "1.53"
+            },
+            {
+                "invert_lightness": true
+            }
+        ]
+    }
+]});
+
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+  });
 });
--- server/main.js
+++ server/main.js
@@ -1,10 +1,13 @@
 var Future = Npm.require( 'fibers/future' ); 
 var Twit = Meteor.npmRequire('twit');
 var randomTime = (1000 * 60 * 3) + (Math.random()* 1000 * 60 * 2) + (Math.random() * 100);
+//var randomTime = 5000;
 
-var profileScreenName = "anecdotalUK";
-var scrapeScreenName = "jar4d";
-var scrapeResults = 10;
+
+ profileScreenName = "anecdotalUK";
+ scrapeScreenName = "jar4d";
+ scrapeResults = 10;
+ stopped = true;
 
 T = new Twit({
   consumer_key:         'WRIK81ClYsbHpNk6hApkzaEBm', // API key
@@ -13,65 +16,119 @@ T = new Twit({
   access_token_secret:  'sIPy4O6lT1bEnhu4rFVPiJwmHx9Ry1f6wktBlLzR2pPdB'
 });
 
+Meteor.startup(function(){
+
+  Profile.remove({}); //purge
+  Tweets.remove({}); //purge
+
+
+         var parsedData = ({   
+          'profileID':"1",
+          'stopped':"stopped",
+          'screen_name': null,
+          'id': null,
+          'description': null,
+          'favourites_count': null,
+          'followers_count': null,
+        'friends_count': null,
+        'listed_count': null,
+        'profile_banner_url': null,
+        'profile_image_url': null,
+        });
+        Profile.insert(parsedData);
+
+  getProfile(profileScreenName);
+  getFriends(profileScreenName);
+  getFollowers(profileScreenName);
+
+  //refresh profile every 5 mins
+  Meteor.setInterval(function(){getProfile(profileScreenName);}, 1000*60*5);
+
+
+});
+
 Meteor.methods({
 	scrapeContactsMethod: function(scrapeScreenName, scrapeResults){
 	 scrapeContacts(scrapeScreenName, scrapeResults);
 
-	}
-});
+	},
 
-Meteor.startup(function(){
-	getProfile(profileScreenName);
-});
+  go: function(){
+      console.log("running scripts...");        
+      createInterval = Meteor.setInterval(function(){
+                                                      create();
+                                                    }, randomTime);
+      cleanInterval = Meteor.setInterval(function(){
+                                                    cleanFriendsLoop();
+                                                  }, randomTime);
 
+  },
+
+  stop: function(){
+      console.log("running clear interval scripts...");    
+      Meteor.clearTimeout(createInterval);
+      Meteor.clearTimeout(cleanInterval);
+  }
+
+
+
+});
 
 
 
 
 function create(){
-	var scrapeId = Tweets.findOne({},{scrapeId: {$exists:true}}).scrapeId;
+  console.log("running create loop");
+	var scrapeId = Scraped.findOne({},{scrapeId: {$exists:true}}).scrapeId;
 
 	if(scrapeId.length === 0){//we need new friends. Alert!
-
+    console.log("run out of scrapeIds. Reload please.");
 	}else
 	{
 	addScrapedFriend(scrapeId);
-	cleanFriendsLoop();
 	}
-
-
-
-
-
 	//check if there are friends available...
 }
 
 
 function cleanFriendsLoop(){ //destroyId
+console.log("running cleanFriendsLoop");
+
 //find all following
 // friends we follow, followers follow us
-var followers = Tweets.find({},{followers: {$exists:true}}).followers;
-var friends = Tweets.find({friends:{$exists:true}}).friends;
+var followers = Tweets.find({},{followers: {$exists:true}}).count();
+var friends = Tweets.find({friends:{$exists:true}}).count();
+console.log("followers: ", followers);
+console.log("friends: ", friends);
 
+
+if (followers == 0){
+  //import followers
+  console.log("running getFollowers");
+  getFollowers(profileScreenName);
+}
+else if (friends == 0){
+  //import friends
+  console.log("running getFriends");
+  getFriends(profileScreenName);
+}
+else{//iterate through them
+console.log("asking to run cleanFriends");
 for(var i = 0; i < friends.length; i++){
-	var compare = Tweets.find({followers:friends[i]});
-	//finds whether a friend is also a follower.
-	if(compare.length === 0){//no match, not a follower
-		destroyId = friends[i];
-		cleanFriends(destroyId);
-	}else{//is a follower. 
+  var compare = Tweets.find({followers:friends[i]});
+  //finds whether a friend is also a follower.
+    if(compare.length === 0){//no match, not a follower
+      destroyId = friends[i];
+      console.log("asking to run cleanFriends to destroy ", destroyId);
 
-	}
+      cleanFriends(destroyId);
+    }else{//is a follower. 
 
-
-
+    }
+}
 }
 
 }
-
-
-
-
 
 
 function getProfile(profileScreenName){
@@ -88,7 +145,7 @@ T.get('users/show', { screen_name:profileScreenName},
        var parsedData = ({   
        	'screen_name': data.screen_name,
         'id': data.id,
-		'description': data.description,    
+		    'description': data.description,    
         'favourites_count': data.favourites_count,
         'followers_count': data.followers_count,
         'friends_count': data.friends_count,
@@ -96,7 +153,7 @@ T.get('users/show', { screen_name:profileScreenName},
         'profile_banner_url': data.profile_banner_url,
         'profile_image_url': data.profile_image_url,
         });
-        Profile.insert(parsedData);
+        Profile.update({profileID:"1"}, parsedData);
         console.log("updated profile");  
        console.log(parsedData);
       
@@ -125,7 +182,7 @@ T.get('followers/ids', { screen_name:scrapeScreenName, count: scrapeResults},
             var parsedData = ({       
  	            'scrapeId': data.ids[i]     
             });
-	            Tweets.insert(parsedData);
+	            Scraped.insert(parsedData);
 	            console.log("inserted new target", data.ids[i] );  
               }
         }
@@ -138,7 +195,7 @@ T.get('followers/ids', { screen_name:scrapeScreenName, count: scrapeResults},
 
 function getFriends(profileScreenName){ //will only work up to 5000
 var future = new Future();
-T.get('friends/ids', { screen_name:'profileScreenName'},  
+T.get('friends/ids', { screen_name:profileScreenName},  
     Meteor.bindEnvironment(function(error, data, response ) {
       if(error){
         future.return( error );
@@ -166,7 +223,7 @@ T.get('friends/ids', { screen_name:'profileScreenName'},
 
 function getFollowers(profileScreenName){ //will only work up to 5000
 var future = new Future();
-T.get('followers/ids', { screen_name:'profileScreenName'},  
+T.get('followers/ids', { screen_name:profileScreenName},  
     Meteor.bindEnvironment(function(error, data, response ) {
       if(error){
         future.return( error );
@@ -182,7 +239,7 @@ T.get('followers/ids', { screen_name:'profileScreenName'},
 	            Tweets.insert({       
 	            'followers': data.ids[i]     
 	            });
-	            console.log("inserted new friend", data.ids[i] );  
+	            console.log("retrieved follower", data.ids[i] );  
               }
         }
       
@@ -193,19 +250,23 @@ T.get('followers/ids', { screen_name:'profileScreenName'},
 }
 
 function addScrapedFriend(scrapeId){
+console.log("running addScrapedFriend");
+
 var future = new Future();
 
 T.post('friendships/create', {user_id: scrapeId},  
     Meteor.bindEnvironment(function(error, data, response ) {
       if(error){
         future.return( error );
+        console.log("for user_id: ", scrapeId);
         console.log("couldnt retrieve friends", error);
+        Scraped.remove({'scrapeId':scrapeId});
 
       } else if(!error && 200 == response.statusCode){
        //console.log(data);
        future.return( response );
         console.log("Got a new friend ", data.screen_name);
-        Tweets.remove({'scrapeId':scrapeId});
+        Scraped.remove({'scrapeId':scrapeId});
         }
       
     })
@@ -223,7 +284,7 @@ T.post('friendships/destroy', {user_id: destroyId},
         console.log("couldnt remove friend", error);
 
       } else if(!error && 200 == response.statusCode){
-       //console.log(data);
+       console.log(data);
        future.return( response );
         console.log("Removed a friend", data.screen_name);
         Tweets.remove({'friends':destroyId});
